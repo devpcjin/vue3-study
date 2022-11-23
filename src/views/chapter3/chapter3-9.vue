75e4debc9045dba1a474dc8c0c73299e29b5bfe5
pcjin22 <pcjin@blockchain.co.kr>
2022-11-23 18:05:10 +0900

    Chapter3-9
    값 증가시키기
    - 함수, ref, method옵션 을 활용한 값 증가 시키기

---
+++ src/views/chapter3/chapter3-9.vue
@@ -0,0 +1,43 @@
+<template>
+    <p>{{ counter + counter2 }}</p>
+    <!--    ref를 참조하여 직접 값 증가   -->
+    <button @click="counter++">클릭하면 숫자가 올라갑니다.</button>
+    <!--    함수를 통해 값 증가     -->
+    <button @click="onClick">클릭하면 숫자가 올라갑니다.</button>
+    <button @click="onClick2">클릭하면 숫자가 올라갑니다.</button>
+</template>
+
+<script>
+    import { ref } from 'vue'
+
+    export default {
+        data() {
+            return {
+                counter2: 0,
+            }
+        },
+        setup() {
+            let counter = ref(0)
+
+            const onClick = (evt) => {
+                if (evt) {
+                    evt.preventDefault()
+                    counter.value++
+                }
+            }
+            return {
+                counter,
+                onClick,
+            }
+        },
+        // methods옵션에 선언된 함수는 data 함수옵션에 선언된 변수만 접근 가능
+        methods: {
+            onClick2: function (evt) {
+                if (evt) {
+                    evt.preventDefault()
+                }
+                this.counter2++
+            },
+        },
+    }
+</script>
