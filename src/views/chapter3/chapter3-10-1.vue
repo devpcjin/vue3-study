33cc0207e82b91247880434236caf51f4200d07e
pcjin22 <pcjin@blockchain.co.kr>
2022-11-24 09:33:57 +0900

    chapter3-10
    - v-if와 v-show

---
+++ src/views/chapter3/chapter3-10-1.vue
@@ -0,0 +1,31 @@
+<template>
+    <input type="button" id="toggle" value="toggle" @click="toggle()">
+    <h4>key의 값 {{ key }}</h4>
+    <div>
+        <h3>v-if</h3>
+        <div v-if="key">
+            v-if
+        </div>
+    </div>
+    <div>
+        <h3>v-show</h3>
+        <div v-show="key">
+            v-show
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                key: true
+            }
+        },
+        methods: {
+            toggle: function () {
+                this.key = !this.key
+            }
+        },
+    }
+</script>
---
+++ src/views/chapter3/chapter3-10.vue
@@ -0,0 +1,25 @@
+<template>
+    <p>{{ counter }}</p>
+    <p v-if="counter < 5">5보다 작습니다.</p>
+    <!--    v-if는 조건이 변경될 때마다 해당 엘리먼트를 다시 그려야함
+            v-show는 조건자주 변경되어도 매우 빠른 전환이 일어남 - 조건에 맞지 않는 엘리먼트는 hide 처리   -->
+
+    <!--    v-if는 조건에 따라 컴포넌트가 실제로 제거되고 생성
+             v-show 는 단순히 css 의 display 속성만 변경   -->
+    <p v-else>5와 같거나 큽니다.</p>
+    <button @click="counter++">클릭하면 숫자가 올라갑니다.</button>
+</template>
+
+<script>
+    import { ref } from 'vue'
+
+    export default {
+        // Composition API
+        setup() {
+            let counter = ref(0)
+            return {
+                counter,
+            }
+        },
+    }
+</script>
