7f6e5c1f558b5cca43ec38313abd14fff60405d8
pcjin22 <pcjin@blockchain.co.kr>
2022-11-24 13:43:43 +0900

    example3-15
    전역범위로 선언할 수 있게 해주는 app 인스턴스 매서드

--- src/main.js
+++ src/main.js
@@ -1,6 +1,28 @@
-import { createApp } from 'vue'
+import {  ref } from 'vue'
+import { createApp } from 'vue/dist/vue.esm-bundler';
 import './style.css'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.component('counter', {
+    // provide 값을 불러 사용할 수 있다.
+    inject: ['counter_header'],
+    setup() {
+        const count = ref(0)
+        return { count }
+    },
+    template: `
+        <span>{{ counter_header }}</span>
+        <button @click="count++" v-text="count" />
+        <hr/>
+    `,
+})
+
+app.config.globalProperties.title = 'Vue 3 Demo'
+app.provide('counter_header', 'Counter ')
+
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/chapter3/example3-15.vue
@@ -0,0 +1,8 @@
+<template>
+    <div id="app">
+        <p>{{ title }}</p>
+        <counter></counter>
+        <counter></counter>
+        <counter></counter>
+    </div>
+</template>
