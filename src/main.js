22794054ea1a16c791d212a398538d41d483f9e8
pcjin22 <pcjin@blockchain.co.kr>
2022-11-24 14:30:04 +0900

    example3-17
    Non-prop 속성

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 
 const app = createApp(App)
 
+// example3-15
 app.component('counter', {
     // provide 값을 불러 사용할 수 있다.
     inject: ['counter_header'],
@@ -17,6 +18,69 @@ app.component('counter', {
         <span>{{ counter_header }}</span>
         <button @click="count++" v-text="count" />
         <hr/>
+    `,
+})
+
+// example3-16
+app.component ('MyComponent', {
+    props: {
+        title: {
+            type: String,
+            required: true,
+            validator: function (value) {
+                return typeof value == String
+            },
+        },
+        data: {
+            type: Object,
+            default: () => {
+                return []
+            },
+        },
+    },
+    template: `
+        <h3 v-text="title"/>
+        <span v-for="i in data" :key="i">{{ i }}</span>
+    `,
+})
+
+//example3-17
+app.component('Component', {
+    setup(props,context) {
+        const msg = context.attrs.msg
+        return {
+            msg,
+        }
+    },
+    template: `
+        <Component3></Component3>
+    `,
+})
+
+app.component('Component2', {
+    inheritAttrs: false,
+    setup(props,context) {
+        const msg = context.attrs.msg
+        return {
+            msg,
+        }
+    },
+    template: `
+        <Component3></Component3>
+    `,
+})
+
+app.component('Component3', {
+    setup(props,context) {
+        const msg = context.attrs.msg
+        return {
+            msg,
+        }
+    },
+    template: `
+        <div>
+            <p>@Component: {{ $attrs.msg }}</p>
+        </div>
     `,
 })
 
---
+++ src/views/chapter3/example3-17.vue
@@ -0,0 +1,5 @@
+<template>
+    <Component msg="Message"></Component>
+    <Component2 msg="Message"></Component2>
+    <Component3 msg="Message"></Component3>
+</template>
